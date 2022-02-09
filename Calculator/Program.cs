76c93c4adf99153884258a338ad3dca25f1ad969
Elol765432 <ponce.josegael@gail.com>
2022-02-08 20:05:50 -0600

    Calculator with C#

---
+++ Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Calculator
+{
+    class calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresa un operador:");
+            string op = Console.ReadLine();
+            Console.WriteLine("Ingresa otro número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }else
+            {
+                Console.WriteLine("Operador Invalido"); 
+            }
+
+        
+        
+        }
+    }
+}
